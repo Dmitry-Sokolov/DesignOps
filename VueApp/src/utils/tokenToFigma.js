cfe6719c113c9e89beeaa14700208e4721243d59
Dmytro Sokolov <xcdxster@gmail.com>
2024-02-27 11:09:27 +0200

    added mock for figma api

---
+++ VueApp/src/utils/tokenToFigma.js
@@ -0,0 +1,75 @@
+// /*
+// GET REQUEST
+// */
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+// const fs = require('fs')
+//
+// const getURL ='https://api.figma.com/v1/files/JR5tmX06VVx9DLFKLRFOIK/variables/local/'
+//
+// async function getData(){
+//     const request = new Request(getURL, {
+//         headers: {
+//             'X-Figma-Token': 'figd_k_Ax7wIBOkYxnNgKuyi5zZseRf2I-IPilo87guXX',
+//         }
+//     })
+//     const response = await fetch(request)
+//
+//     const jsonData = await response.json()
+//     const data = JSON.stringify(jsonData, null, 4)
+//
+//     fs.writeFile('../json/variable.json', data, 'utf8', (err) => {
+//         if(err){
+//             console.log('Error')
+//         }
+//     })
+//
+// }
+//
+// getData()
+
+/*
+    POST REQUEST
+*/
+
+const postURL ='https://api.figma.com/v1/files/JR5tmX06VVx9DLFKLRFOIK/variables/'
+const variableCollectionId = "VariableCollectionId:1:2";
+
+const testData = {
+    "variables": [
+        {
+            "action": "CREATE",
+            "name": "colors/test-primary-blue",
+            "variableCollectionId": variableCollectionId,
+            "resolvedType": "COLOR",
+            "description": "",
+            "hiddenFromPublishing": false,
+            "valuesByMode": {
+                "1:0": {
+                    "r": 0,
+                    "g": 0.5308057069778442,
+                    "b": 1,
+                    "a": 1
+                }
+            },
+        }
+    ]
+}
+
+async function postData(){
+ //   const jsonResponse = await fetch('./postVariable.json')
+ //    const jsonData = await jsonResponse.json()
+ //
+    const response = await fetch(postURL, {
+        method: 'POST',
+        body: JSON.stringify(testData),
+        headers: {
+            'X-Figma-Token': 'figd_k_Ax7wIBOkYxnNgKuyi5zZseRf2I-IPilo87guXX',
+            'Content-Type': 'application/json'
+        },
+    })
+    const jsonData = await response.json()
+    console.log(jsonData)
+}
+
+postData()
